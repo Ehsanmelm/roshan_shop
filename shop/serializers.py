65f59ef62e1ebb5c78901127ecfd46cab8b3a029
Ehsan <ehsanmelm82@gmail.com>
2024-11-20 20:18:54 +0330

    feat : setting up serializer for product , category , shop cart

---
+++ shop/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import CategoryModel , ProductsModel , ShopCartModel
+from core.serializers import UserRegisterSerializer
+class CategorySerializer(ModelSerializer):
+
+    class Meta:
+        model = CategoryModel
+        fields = ['name']
+
+
+
+class ProductsSerializer(ModelSerializer):
+    # category = serializers.SerializerMethodField(method_name= "category_fields")
+    class Meta:
+        model = ProductsModel
+        fields = ['id' , 'name' , 'description' , 'price' , 'capacity' , 'category']
+
+    # def category_fields(self,obj):
+        # return obj.category.name
+
+class ShopCartSerializer(ModelSerializer):
+    user = UserRegisterSerializer(read_only = True)
+    product = ProductsSerializer()
+
+    class Meta:
+        model = ShopCartModel
+        fields = ['id' , 'user' , 'product']
